f79b14dbb4534903062d3d8298b7fd8b910df925
domingmamalio-create <domingmamalio@gmail.com>
2026-09-21 22:23:20 +0800

    Create AppInfo.kt

---
+++ app/src/main/java/com/drayvokz/virtual/AppInfo.kt
@@ -0,0 +1,11 @@
+```kotlin
+package com.drayvokz.virtual
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val packageName: String,
+    val label: String,
+    val icon: Drawable?
+)
+```
